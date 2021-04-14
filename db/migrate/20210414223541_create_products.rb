3f15f45df66448552f26b086b9f842549d31b259
Alejandra <marialesr.17@gmail.com>
2021-04-14 17:03:43 -0600

    created model scaffold

---
+++ db/migrate/20210414223541_create_products.rb
@@ -0,0 +1,11 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :name_product
+      t.integer :price
+      t.integer :amount
+
+      t.timestamps
+    end
+  end
+end
